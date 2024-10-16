23f5f23bc43a298aafccde8c771289aeaa01474a
Alessandra Nébias <anebias@hotmail.com>
2024-10-16 20:02:38 -0300

    Resolução desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Instalando smartfone Iphone...");
+
+Smartphone iphone_15 = new Iphone(numero: "99998877", modelo: "iphone 15", imei: "A45BX45", memoria: 128);
+
+iphone_15.Ligar();
+iphone_15.InstalarAplicativo("APP do banco");
+
+Console.WriteLine("\n\n");
+
+Console.WriteLine("Instalando smartfone Nokia...");
+
+Smartphone nokia_1020 = new Nokia(numero: "99998877", modelo: "iphone 15", imei: "A45BX45", memoria: 128);
+
+nokia_1020.Ligar();
+nokia_1020.InstalarAplicativo("APP delivery");
